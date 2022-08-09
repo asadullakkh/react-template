bcddda69dbc4e8b1819775d5516169874a52d6c9
Asadbek Eshboev <a.eshboev@uic.group>
2022-08-09 14:36:17 +0500

    first commit

---
+++ src/components/Hello.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <p style={{background: 'red', color: 'white', fontWeight: 900}} className="footer">Copyright © 2021 Asadullakh. All rights reserved.</p>;
+};
+
+export default Footer;
