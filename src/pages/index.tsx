453f6ac2a64d250a194f19436d1210b4989acfa5
Asadbek <a.eshboev@uic.group>
2022-08-10 00:36:27 +0500

    feat: add custom classes to tailwind css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 export const Home = () => {
   return (
-    <div>
+    <div className="flex-center-center">
       <h1 className="text-3xl -500:text-[yellow] text-red-500 font-bold underline">
         Hello world!
       </h1>
