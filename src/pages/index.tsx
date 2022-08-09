a6b556313d11fddbe927eacf316094f23e3f4350
Asadbek <a.eshboev@uic.group>
2022-08-10 00:53:56 +0500

    feat: material ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
+import Button from "@mui/material/Button";
+
 export const Home = () => {
   return (
     <div className="flex-center-center">
       <h1 className="text-3xl -500:text-[yellow] text-red-500 font-bold underline">
         Hello world!
       </h1>
+      <Button variant="contained">Hello World</Button>
     </div>
   );
 };
